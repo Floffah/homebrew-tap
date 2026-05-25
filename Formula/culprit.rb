4c9f3dda8b8890516aa9a917b2ba011142ea14fe
Ramsay <floffah@floffah.dev>
2026-05-25 20:01:41 +0000

    culprit: add 0.0.1-beta.1 bottle.

--- Formula/culprit.rb
+++ Formula/culprit.rb
@@ -5,6 +5,12 @@ class Culprit < Formula
   sha256 "8013b17434583615dadfcc934db39ee08e1dc35485cb5a8dfd17c82e76e2ec09"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Floffah/homebrew-tap/releases/download/culprit-0.0.1-beta.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "4d7a0f2c0f52507faa452129fec411b5dfbdb2e6706b1487a955476b200df0ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a21f0721d8cde684834598b2e790a4e8d415fc35b7c609bc463e25e2039937ac"
+  end
+
   depends_on "go" => :build
 
   def install
